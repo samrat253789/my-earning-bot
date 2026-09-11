409ad6bd0f199f4411644f71dd1d7322d38c97f8
samrat253789 <homayrapear62@gmail.com>
2026-09-12 05:25:03 +0530

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyLE5OlS0GJwojkgq3-pXi2EVRbnq5zRg",
+  authDomain: "fir-cdb95.firebaseapp.com",
+  projectId: "fir-cdb95",
+  storageBucket: "fir-cdb95.firebasestorage.app",
+  messagingSenderId: "906148426337",
+  appId: "1:906148426337:web:05312aad643fd89d0c7020",
+  measurementId: "G-HPVSQC5ZVJ"
+};
